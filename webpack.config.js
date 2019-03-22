ff49406be6c608bd7899aff22920ac05fcfc3853
Sabatr <bnguyen12152@gmail.com>
2019-03-23 12:22:10 +1300

    Added dependencies

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const webpack = require('webpack');
+const path = require('path');
+module.exports = {
+    entry: path.resolve(__dirname,'./src/index.js'),
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "bundle.js",
+        
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+            }
+        ]
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env': {
+                'NODE_ENV' : JSON.stringify('production')
+            }
+        })
+    ]
+};
