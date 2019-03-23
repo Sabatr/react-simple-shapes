4f1b4ac1e11711dd6efe49dd99bdd044b0a64383
Sabatr <bnguyen12152@gmail.com>
2019-03-23 17:00:51 +1300

    Base entrypoints

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+const components = () => {
+    return (
+        <div>
+        
+        </div>
+    )
+}
+
+ReactDom.render(<this.components/>,document.getElementById('root'));
