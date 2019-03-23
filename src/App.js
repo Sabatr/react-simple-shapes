f0422ba0cf7dda4bc0aa0f417d9bbcb5174aab4a
Sabatr <bnguyen12152@gmail.com>
2019-03-23 17:04:23 +1300

    Added a test

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-
+import {Test} from './components/Test/Test'
+/**
+ * Component used to view the components
+ */
 const components = () => {
     return (
         <div>
-        
+            <Test/>
         </div>
     )
 }
--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,8 @@
+import Test from './Test/Test'
+
+/**
+ * Exporting all of the necessary components
+ */
 module.exports = {
-    
+    Test
 }
