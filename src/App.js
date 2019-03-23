f50a2b4e4bdff58fd34c85e9de35dd8d33a0301c
Sabatr <bnguyen12152@gmail.com>
2019-03-23 17:11:26 +1300

    Added test

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-import {Test} from './components/Test/Test'
+import Test from './components/Test/Test'
 /**
  * Component used to view the components
  */
-const components = () => {
-    return (
-        <div>
-            <Test/>
-        </div>
-    )
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <Test />
+            </div>
+        );
+    }
 }
 
-ReactDom.render(<this.components/>,document.getElementById('root'));
+ReactDom.render(<App/>,document.getElementById('root'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const webpack = require('webpack');
 const path = require('path');
 module.exports = {
-    entry: path.resolve(__dirname,'./src/index.js'),
+    entry: path.resolve(__dirname,'./src/App.js'),
     output: {
         path: path.resolve(__dirname, "dist"),
         filename: "bundle.js",
